a5c66c39311404480f6c69224b1b9a1512bfcfd8
Miguel Javier Dev <m.javierdev@gmail.com>
2023-05-03 11:35:26 -0300

    modificacion en fetch

--- JS/main.js
+++ JS/main.js
@@ -12,7 +12,7 @@ closeMenu.addEventListener("click", () => {
 
 let productos = [];
 
-fetch("./js/productos.json")
+fetch("../JS/productos.json")
   .then((response) => response.json())
   .then((data) => {
     productos = data;
